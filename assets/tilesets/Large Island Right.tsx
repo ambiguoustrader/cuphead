b27b229e27c91c103e6bd5dc590bf017f81244e2
ambiguoustrader <ambiguoustrader@gmail.com>
2026-01-23 20:31:50 +0300

    sourcach and changes (2)

---
+++ assets/tilesets/Large Island Right.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Large Island Right" tilewidth="1240" tileheight="1944" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/map/Large Island/Right/Main/world1_large_island_main_02.png" width="1240" height="1944"/>
+ </tile>
+</tileset>
